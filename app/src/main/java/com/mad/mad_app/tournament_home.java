44dc9edb4ef5ab05a05a2cab09ed7d5f1f6a151b
nipunbanadara <nipun.banadara@outlook.com>
2021-08-12 18:25:11 +0530

    2 tournament pages connected to home

---
+++ app/src/main/java/com/mad/mad_app/tournament_home.java
@@ -0,0 +1,31 @@
+package com.mad.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class tournament_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tournament_home);
+        Intent intent = getIntent();
+    }
+
+    public void tournament_user_sel(View view){
+
+        Intent intent = new Intent(this, tournament_user_selected_t.class);
+        startActivity(intent);
+
+    }
+
+    public void home(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
--- app/src/main/java/com/mad/mad_app/tournament_user_selected_t.java
+++ app/src/main/java/com/mad/mad_app/tournament_user_selected_t.java
@@ -5,22 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class tournament_user_selected_t extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tournament_user_selected_t);
+        Intent intent = getIntent();
     }
+    public void home(View view){
 
-    public void tournament_home(View view){
-
-        Intent intent = new Intent(this, tournament_home.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
     }
-
 
 }
