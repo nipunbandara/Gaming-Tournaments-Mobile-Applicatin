fef4551637581c54c077d8b0d2b0ac8ba882cc75
nuduja <nudujabandara@gmail.com>
2021-08-14 00:38:40 +0530

    match create, match update, match org list, match player list

---
+++ app/src/main/java/com/mad/mad_app/match_details.java
@@ -0,0 +1,14 @@
+package com.mad.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class match_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_details);
+    }
+}
