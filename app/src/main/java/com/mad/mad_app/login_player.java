218362618c7b0b6645b9fb12b2720bb9c472e7f5
nipunbandara <nipun.bandara@outlook.com>
2021-08-14 21:55:04 +0530

    login user, org connected to their home pages

--- app/src/main/java/com/mad/mad_app/login_player.java
+++ app/src/main/java/com/mad/mad_app/login_player.java
@@ -2,7 +2,9 @@ package com.mad.mad_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login_player extends AppCompatActivity {
 
@@ -12,4 +14,10 @@ public class login_player extends AppCompatActivity {
         setContentView(R.layout.activity_login_player);
     }
 
+    public void user_home(View view){
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
 }
