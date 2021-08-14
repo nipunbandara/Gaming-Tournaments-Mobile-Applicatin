886d007b41de01cbd202da429cc8a09a8e37a6f8
nipunbandara <nipun.bandara@outlook.com>
2021-08-14 14:42:54 +0530

    fixed gradle build errors

--- app/src/main/java/com/mad/mad_app/Match_create.java
+++ app/src/main/java/com/mad/mad_app/Match_create.java
@@ -9,6 +9,6 @@ public class Match_create extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_match_create);
+        setContentView(R.layout.activity_match_org_create);
     }
 }
