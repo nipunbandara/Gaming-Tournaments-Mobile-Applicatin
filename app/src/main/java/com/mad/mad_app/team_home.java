5312bf2073a11ff9d0705a837cac21d68a56d1be
randi_.99 <randimalfdo@gmail.com>
2021-08-14 13:31:28 +0530

    team_home added

---
+++ app/src/main/java/com/mad/mad_app/team_home.java
@@ -0,0 +1,14 @@
+package com.mad.mad_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class team_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_home);
+    }
+}
