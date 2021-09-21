ece7608c5c2c53639eb83665f71d372469d4d015
nipunbandara <nipun.bandara@outlook.com>
2021-09-21 14:23:43 +0530

    login added

--- app/src/main/java/com/mad/mad_app/signup_player.java
+++ app/src/main/java/com/mad/mad_app/signup_player.java
@@ -1,17 +1,131 @@
 package com.mad.mad_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class signup_player extends AppCompatActivity {
+
+    private EditText email, password, userName, fullName;
+    private Button registerBtn;
+    private TextView registerQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+    private DatabaseReference userDatabaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_player);
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        userName = findViewById(R.id.username);
+        fullName = findViewById(R.id.fullName);
+        registerBtn = findViewById(R.id.signup_player);
+        registerQn = findViewById(R.id.registerQn);
+        loader = new ProgressDialog(this);
 
         this.setTitle("Signup - Player");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        registerQn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup_player.this, login_player.class);
+                startActivity(intent);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userNameString = userName.getText().toString();
+                String fullNameString = fullName.getText().toString();
+                String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if (TextUtils.isEmpty(emailString)){
+                    email.setError("Email is required");
+                }
+                if (TextUtils.isEmpty(passwordString)){
+                    password.setError("Password is required");
+                }
+                if (TextUtils.isEmpty(userNameString)){
+                    userName.setError("User Name is required");
+                }
+                if (TextUtils.isEmpty(fullNameString)){
+                    fullName.setError("Full Name is required");
+                }
+                else {
+
+                    loader.setMessage("Registering you...");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    mAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()){
+                                String error = task.getException().toString();
+                                Toast.makeText(signup_player.this, "Error" + error, Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                String currentUserId = mAuth.getCurrentUser().getUid();
+                                userDatabaseRef = FirebaseDatabase.getInstance().getReference()
+                                        .child("players").child(currentUserId);
+                                HashMap userInfo = new HashMap();
+                                userInfo.put("id", currentUserId);
+                                userInfo.put("email", emailString);
+                                userInfo.put("userName", userNameString);
+                                userInfo.put("fullName", fullNameString);
+
+                                userDatabaseRef.updateChildren(userInfo).addOnCompleteListener(new OnCompleteListener() {
+                                    @Override
+                                    public void onComplete(@NonNull Task task) {
+                                        if (task.isSuccessful()){
+                                            Toast.makeText(signup_player.this, "Data set Successfully", Toast.LENGTH_SHORT).show();
+                                        }else {
+                                            Toast.makeText(signup_player.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                                        }
+
+                                        finish();
+                                        //loader.dismiss();
+                                    }
+                                });
+
+                                Intent intent = new Intent(signup_player.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                loader.dismiss();
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
