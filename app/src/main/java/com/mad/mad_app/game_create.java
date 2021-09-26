76b46e57f16c5e8004cfa8fd93f17e5bec0362dd
maneesha <maneeshakaravita@gmail.com>
2021-09-26 15:18:59 +0530

    game pages updated

--- app/src/main/java/com/mad/mad_app/game_create.java
+++ app/src/main/java/com/mad/mad_app/game_create.java
@@ -1,46 +1,134 @@
 package com.mad.mad_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Task;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 
 public class game_create extends AppCompatActivity {
+    private EditText gname, gregion, gdescription, gminteams, gmaxteams;
+
+    private Button registerBtn;
+    private TextView registerQn;
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+    private DatabaseReference userDatabaseRef;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_create);
-        Intent intent = getIntent();
+
 
 
         this.setTitle("Create Game");
+
+        gname = findViewById(R.id.gnamekey);
+        gregion = findViewById(R.id.update_region);
+        gdescription = findViewById(R.id.update_des);
+        gminteams = findViewById(R.id.update_min);
+        gmaxteams = findViewById(R.id.update_max);
+
+        registerBtn = findViewById(R.id.button2);
+
+        loader = new ProgressDialog(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String gnameString = gname.getText().toString();
+                String gregionString =gregion.getText().toString();
+                String gdescriptionString =gdescription.getText().toString();
+                String gminteamsString =gminteams.getText().toString();
+                String  gmaxteamsString = gmaxteams.getText().toString();
+
+
+                if (TextUtils.isEmpty(gnameString)) {
+                    gname.setError("gname is required");
+                }
+                if (TextUtils.isEmpty(gregionString)) {
+                    gregion.setError("Password is required");
+                }
+                if (TextUtils.isEmpty(gdescriptionString)) {
+                    gdescription.setError("User Name is required");
+                }
+                if (TextUtils.isEmpty( gminteamsString)) {
+                    gminteams.setError("Full Name is required");
+                }
+                if (TextUtils.isEmpty(gmaxteamsString)) {
+                    gmaxteams.setError("Full Name is required");
+                }
+
+                else {
+
+                    loader.setMessage("Registering you...");
+                    loader.setCanceledOnTouchOutside(false);
+                    loader.show();
+
+                    String currentUserId = mAuth.getCurrentUser().getUid();
+                    //database initialization
+                    userDatabaseRef = FirebaseDatabase.getInstance().getReference()
+                            .child("games");
+
+                    String id  = userDatabaseRef.push().getKey();
+
+                    //using hashmaps to add details to the database
+                    HashMap userInfo = new HashMap();
+                    userInfo.put("id", id);
+                    userInfo.put("oid", currentUserId);
+                    userInfo.put("gname", gregionString);
+                    userInfo.put("gregion", gdescriptionString);
+                    userInfo.put("gdescription",gdescriptionString);
+                    userInfo.put("gminteams", gminteamsString);
+                    userInfo.put("gmaxteams", gmaxteamsString);
+
+                    //allocating data structure to the database
+                    userDatabaseRef.child(id).setValue(userInfo).addOnCompleteListener(new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(game_create.this, "Data set Successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(game_create.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            finish();
+                            //loader.dismiss();
+                        }
+                    });
+
+                    Intent intent = new Intent(game_create.this, organizer_home.class);
+                    startActivity(intent);
+                    finish();
+                    loader.dismiss();
+
+                }
+
+            }
+        });
     }
-    public void game_create_org(View view){
-
-        Intent intent = new Intent(this, game_org_gamelist.class);
-        startActivity(intent);
-    }
-
-    public void tournament_create_org(View view){
-
-        Intent intent = new Intent(this, tournament_org_list.class);
-        startActivity(intent);
-    }
-
-    public void organizer_create_org(View view){
-
-        Intent intent = new Intent(this, organizer_organizer_info.class);
-        startActivity(intent);
-    }
-
-    public void home_org(View view){
-
-        Intent intent = new Intent(this, organizer_home.class);
-        startActivity(intent);
-
-    }
-
 
 }
--- app/src/main/java/com/mad/mad_app/game_update.java
+++ app/src/main/java/com/mad/mad_app/game_update.java
@@ -1,12 +1,41 @@
 package com.mad.mad_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class game_update extends AppCompatActivity {
+
+
+
+    private String id;
+
+    private EditText gname, gregion, gdescription, gminteams, gmaxteams;
+    private Button Btn_update, Btn_cancel;
+    private String gnameString, gregionString, gdescriptionString, gminteamsString,gmaxteamsString;
+
+
+    private FirebaseAuth mAuth;
+    private ProgressDialog loader;
+    private DatabaseReference gameRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,30 +43,95 @@ public class game_update extends AppCompatActivity {
         setContentView(R.layout.activity_game_update);
 
 
-        this.setTitle("Update Game");
-    }
-    public void game_create_org(View view){
+        this.setTitle("Update Tournament");
 
-        Intent intent = new Intent(this, game_org_gamelist.class);
-        startActivity(intent);
-    }
+        Btn_update = findViewById(R.id.update_button);
+        Btn_cancel = findViewById(R.id.cancle_button);
 
-    public void tournament_create_org(View view){
+        Intent myIntent = getIntent();
+        id = myIntent.getStringExtra("MAIN_EXTRA");
 
-        Intent intent = new Intent(this, tournament_org_list.class);
-        startActivity(intent);
-    }
+        mAuth = FirebaseAuth.getInstance();
+        gameRef = FirebaseDatabase.getInstance().getReference().child("games").child(id);
 
-    public void organizer_create_org(View view){
+        gameRef.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
 
-        Intent intent = new Intent(this, organizer_organizer_info.class);
-        startActivity(intent);
-    }
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Game tourn = task.getResult().getValue(Game.class);
 
-    public void home_org(View view){
 
-        Intent intent = new Intent(this, organizer_home.class);
-        startActivity(intent);
+
+
+                    gname = findViewById(R.id.update_name);
+                    gregion = findViewById(R.id.update_region);
+                    gdescription = findViewById(R.id.update_des);
+                    gminteams = findViewById(R.id.update_min);
+                    gmaxteams = findViewById(R.id.update_max);
+
+                    gname.setText(tourn.getGname());
+                    gregion.setText(tourn.getGregion());
+                    gdescription.setText(tourn.getGdescription());
+                    gminteams.setText(tourn.getGminteams());
+                    gmaxteams.setText(tourn.getGmaxteams());
+
+
+
+
+                }
+            }
+        });
+        Btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gname = findViewById(R.id.update_name);
+                gregion = findViewById(R.id.update_region);
+                gdescription = findViewById(R.id.update_des);
+                gminteams = findViewById(R.id.update_min);
+                gmaxteams = findViewById(R.id.update_max);
+
+                gnameString = gname.getText().toString();
+                gregionString =gregion.getText().toString();
+                gdescriptionString =gdescription.getText().toString();
+                gminteamsString =gminteams.getText().toString();
+                gmaxteamsString = gmaxteams.getText().toString();
+                //using hashmaps to add details to the database
+                HashMap userInfo = new HashMap();
+                userInfo.put("gname", gnameString);
+                userInfo.put("gregion", gregionString);
+                userInfo.put("gdescription", gdescriptionString);
+                userInfo.put("gminteams", gminteamsString);
+                userInfo.put("gmaxteams", gmaxteamsString);
+
+                //allocating data structure to the database
+                gameRef.updateChildren(userInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            //toast when the update is successful
+                            Toast.makeText(game_update.this, "Updated successfully", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(game_update.this, game_org_gamelist.class);
+                            startActivity(intent);
+                            finish();
+                            loader.dismiss();
+
+                        }else {
+                            Toast.makeText(game_update.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+
+                        finish();
+
+                    }
+                });
+
+
+            }
+        });
 
     }
 
